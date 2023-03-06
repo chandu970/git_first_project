1c6a27506a75a2821952f895a231794a8543a8b5
chandu970 <cchandu6136@gmail.com>
2023-03-06 22:07:52 +0530

    first commit

---
+++ video/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Chandu(request):
+    return HttpResponse('<h1 style="color:Blue;"><marquee>sorry vem 🙇🏽🙇🏽🙇🏽!!!</marquee></h1>')
